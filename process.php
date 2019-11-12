c2fb98410409ea18b7b989ff01d5530ef03f3a2a
Kunal Gaurav <kunalgaurav0405@gmail.com>
2019-11-12 10:29:28 +0530

    php

--- process.php
+++ process.php
@@ -11,7 +11,7 @@
     //send email
     mail($admin_email, "New Form Submission", $message . ' - ' . $phone, "From:" . $email);
     
-    header('Location: http://edgeledger.net/success.html');
+    header('Location: https://kunalgaurav4.github.io/edgeleadersuccess.html');
   }
 
  
